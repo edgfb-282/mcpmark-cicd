1df4e66afdf93dd7aa3d0767a9429b1893c71244
edgfb-282 <mvcvex9m@301490.xyz>
2026-01-02 15:29:04 +0800

    Fix linting errors in src/example.js

--- src/example.js
+++ src/example.js
@@ -1,6 +1,5 @@
-const unusedVar = "this is unused";
-console.log("Hello world");
-let anotherVar = 123
+console.log('Hello world');
+let anotherVar = 123;
 function test() {
-  return "double quotes"
+  return 'single quotes';
 }
